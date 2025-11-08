612da2d67d585712fa7f7f5873bc0123fb819422
Vaidansh <vaidanshgodbole@gmail.com>
2025-11-09 01:29:05 +0530

    Finished website tutorial.

---
+++ script.js
@@ -0,0 +1,7 @@
+/* open class is added or removed by clicking the two targeted classes */
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
